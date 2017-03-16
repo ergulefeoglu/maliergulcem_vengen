53e99dd28c1d3f0474f5e3276b6d41f8387fe5cd
Ergül Efeoğlu <Ergl Efeolu>
2017-03-16 14:53:10 +0300

    Project update

--- src/shows-model.js
+++ src/shows-model.js
@@ -6,7 +6,7 @@ class ShowsModel {
     }
 
     fetch(cb) {
-        xhr('https://raw.githubusercontent.com/dashersw/erste.js-demo/master/src/static/data/shows.json', (err, data) => {
+        xhr('static/data/shows.json', (err, data) => {
             this.shows = data.slice(0, 20);
             cb(this.shows);
         });
--- src/views/shows/list-item.js
+++ src/views/shows/list-item.js
@@ -11,10 +11,11 @@ class ListItem extends erste.Component {
         return `
 <list-item id="${this.id}" data-show-id="${this.show['_id']}"
 style="background-image: url(static/img/cover/${imgFile})">
-    <show-title>${this.show['title']}</show-title>
-    <show-title><br>${this.show['year']}</show-title>
-    <show-title><br><br>${this.show['num_seasons']}</show-title>
+    <show-title>${this.show['place']}</show-title>
+    <show-title><br>${this.show['date']}</show-title>
+    <show-title><br><br>${this.show['title']}</show-title>
 </list-item>
+<br>
 `;
     };
 
