72a17be4153e729de727d311c8c2e4330bd92a3f
Ergül Efeoğlu <Ergl Efeolu>
2017-03-17 15:41:36 +0300

    Project update

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -1,10 +1,7 @@
 var __ = erste.locale.__;
 
 var ShowsView = require('./shows/shows-view');
-var AboutView = require('./about/about-view');
 var Sidebar = require('./sidebar/sidebar');
-var SignIn = require('./sidebar/signin');
-var SignUp = require('./sidebar/signup');
 var Search = require('./search/search');
 
 class MainView extends erste.TabBar {
@@ -12,51 +9,21 @@ class MainView extends erste.TabBar {
         super();
 
         this.showsView = new ShowsView();
-        this.aboutView = new AboutView();
-        this.signIn = new SignIn();
-        this.signUp = new SignUp();
         this.search = new Search();
-        
-        this.views = [this.showsView, this.search, this.search, this.search, this.signIn, this.aboutView];
+
+        this.views = [this.showsView, this.search];
 
         this.showsView.navBar.menuButtonHandler = () => vm.toggleSidebar();
-
+        this.vm = vm;
         this.hasSidebar = true;
 
-        this.sidebar = new Sidebar();
-        this.sidebar.vm = vm;
-
-        //this.sidebar.on('switchView', e => this.activateItemByName(e.view));
-        this.sidebar.on('switchView', e => {
-            var view;
-
-            switch(e.view) {
-                case 'Sign up':
-                view = this.signUp;
-                break;
-                case 'Sign in':
-                view = this.signIn;
-                break;
-                case 'About':
-                view = this.aboutView;
-                break;
-                default:
-                view = this.showsView;
-            }
-            vm.setCurrentView(view);
-
-        }
-        );
-        this.sidebar.render(document.body);
-        
     }
 
     onAfterRender() {
         super.onAfterRender();
 
-        
+        this.vm.setCurrentView(this.showsView);
 
-        //this.activateItemByName('shows');
     };
 
     template_items() {
